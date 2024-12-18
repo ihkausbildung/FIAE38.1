2af7329d2473d0706ceb8c7d202bb36c0b488919
micha schirmer <micha.schirmer@gmail.com>
2024-12-18 12:26:00 +0100

    ContactDAO

--- contacts/src/main/java/org/example/contacts/model/Contact.java
+++ contacts/src/main/java/org/example/contacts/model/Contact.java
@@ -1,10 +1,18 @@
 package org.example.contacts.model;
 
 public class Contact {
+
+    private int id;
     private String name;
     private String number;
 
     public Contact(String name, String number) {
+        this.name = name;
+        this.number = number;
+    }
+
+    public Contact(int id, String name, String number) {
+        this.id = id;
         this.name = name;
         this.number = number;
     }
@@ -28,5 +36,13 @@ public class Contact {
     @Override
     public String toString() {
         return name+": "+number;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 }
